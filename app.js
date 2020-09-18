0988a920f9f2f3fe76f96eed1c86fb149402d2b0
AdyRock <38184396+AdyRock@users.noreply.github.com>
2020-09-18 23:01:27 +0100

    added degbugger

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict';
+if (process.env.DEBUG === '1') {
+	require('inspector').open(9222, '0.0.0.0', false)
+}
 
 const SomaConnectApp = require( './lib/SomaConnectApp' );
 //require('inspector').open(9229, '0.0.0.0');
